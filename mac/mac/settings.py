f022b0af8a5000560afc7484e30ddb220f6393d4
Bigyan <luitelbigyan344@gmail.com>
2025-02-06 08:23:39 +0545

    AddingMedia

--- mac/mac/settings.py
+++ mac/mac/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#managingMedia
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL= '/media/'
